5e7b7e0a6876aa2f38f4a00b09114382f3dd4c92
21october <youkyung.paris@gmail.com>
2022-11-16 01:37:18 +0900

    mini project update

--- mini_project_sandro.py
+++ mini_project_sandro.py
@@ -3,15 +3,17 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 import openpyxl
+import os,sys
+
 
 #python3 -m streamlit run /Users/youkyung/temp/my_first_repository/my_first_repository/strlit.py
 
-# import streamlit as st
-# import pandas as pd
+import streamlit as st
+import pandas as pd
 
-# form = st.form(key="my_form")
-# url = form.text_input(label="Url을 입력하세요. 이미지와 상품정보가 다운로드 됩니다.")
-# submit_button = form.form_submit_button(label='Submit')
+form = st.form(key="my_form")
+url = form.text_input(label="Url을 입력하세요. 이미지와 상품정보가 다운로드 됩니다.")
+submit_button = form.form_submit_button(label='Submit')
 
 wb = openpyxl.Workbook()
 ws = wb.active
