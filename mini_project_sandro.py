8c49d1fa8aba6cb7799f49407867a33019427944
21october <youkyung.paris@gmail.com>
2022-11-16 10:58:14 +0900

    commit 16/11

--- mini_project_sandro.py
+++ mini_project_sandro.py
@@ -14,6 +14,7 @@ import pandas as pd
 form = st.form(key="my_form")
 url = form.text_input(label="Url을 입력하세요. 이미지와 상품정보가 다운로드 됩니다.")
 submit_button = form.form_submit_button(label='Submit')
+print(url)
 
 wb = openpyxl.Workbook()
 ws = wb.active
